475ad86899e030d8306ca578e14f4acfbabfc656
Zlobina Sofya <Sofya.Zlobina@qsrsystem.ru>
2026-04-18 16:47:59 +0300

    update tab

--- projects/currency-rates/script.js
+++ projects/currency-rates/script.js
@@ -10,27 +10,27 @@ function loadCurrencyRates() {
   Как только получаем данные просиходит скрытие анимации через loader_active
   */
   fetch('https://students.netoservices.ru/nestjs-backend/slow-get-courses')
-      .then(res => res.json())
-      .then(data => {
-          loader.classList.remove('loader_active');
+    .then(res => res.json())
+    .then(data => {
+      loader.classList.remove('loader_active');
 
-          const valutes = data.response.Valute;
-          let html = '';
+      const valutes = data.response.Valute;
+      let html = '';
 
-          for (let code in valutes) {
-              const cur = valutes[code];
+      for (let code in valutes) {
+        const cur = valutes[code];
 
-              html += `
+        html += `
               <div class="item">
                   <div class="item__code">${cur.CharCode}</div>
                   <div class="item__value">${cur.Value.toFixed(2)}</div>
                   <div class="item__currency">руб.</div>
               </div>
               `;
-          }
+      }
 
-          items.innerHTML = html;
-      });
+      items.innerHTML = html;
+    });
 }
 
 document.addEventListener('DOMContentLoaded', loadCurrencyRates);
